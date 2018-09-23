0866e1b3158a026495eeb48b81967b93869fb004
royneal <royneal@gmail.com>
2018-09-22 22:29:28 -0400

    Added coments to test file , modified .ignore

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,22 @@
+/*
+ * @copyright Royneal Rayess
+ * @file test.cpp
+ * @auther Royneal Rayess
+ * @ver1.0.0.0
+ *
+ * @brief A unit test to examine
+ *        PID controller class
+ *
+ */
+
 #include <gtest/gtest.h>
 #include <pidController.hpp>
 
 
+/*
+ * @brief test set to examine individual
+ *        computational steps
+ */
 TEST(PidControllerTest, PIDElementsTest) {
   Pid_controller c(0.01, 0.001, 0.01);
   EXPECT_NEAR(c.compute_step(10), 10, 0.001);
@@ -9,6 +24,9 @@ TEST(PidControllerTest, PIDElementsTest) {
   EXPECT_EQ(c.clipper(10), 10);
 }
 
+/*
+ * @brief test set to examine corner cases
+ */
 TEST(PidControllerTest, PIDpreformaceTest) {
   Pid_controller c(0.01, 0.001, 0.01);
   c.compute(5, 0);
@@ -17,8 +35,13 @@ TEST(PidControllerTest, PIDpreformaceTest) {
   EXPECT_NEAR(c.get_current_state(), 5, 0.001);
 }
 
+/*
+ * @brief test set to examine resilience to input error
+ */
 TEST(PidControllerTest, PIDExceptionsTest) {
   Pid_controller c(0.01, 0.001, 0.01);
   EXPECT_ANY_THROW(c.compute(-5, 0));
+  EXPECT_ANY_THROW(c.compute(-5, -5));
+  EXPECT_ANY_THROW(c.compute(5, -5));
   EXPECT_ANY_THROW(c.compute_step(-5));
 }
